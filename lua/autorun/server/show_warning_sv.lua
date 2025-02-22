5e6851c5e46c2c6cd95f99720d09d718ed934214
Niikbb <asd0l@yandex.ru>
2025-02-22 03:17:53 +0300

    remove SERVER check, not required anymore.

--- lua/autorun/server/show_warning_sv.lua
+++ lua/autorun/server/show_warning_sv.lua
@@ -1,26 +1,24 @@
-if SERVER then
-	-- Create a unique identifier for checking if the player has seen the window.
-	util.AddNetworkString("ShowWelcomeWindow")
+-- Create a unique identifier for checking if the player has seen the window.
+util.AddNetworkString("ShowWelcomeWindow")
 
-	-- Check if the player has already joined the server before
-	hook.Add("PlayerInitialSpawn", "ShowWindowOncePerPlayer", function(ply)
-		if ply:IsBot() then return end
+-- Check if the player has already joined the server before
+hook.Add("PlayerInitialSpawn", "ShowWindowOncePerPlayer", function(ply)
+	if ply:IsBot() then return end
 
-		local window_file = "homigrad/window_showed.txt"
-		if not file.Exists(window_file, "DATA") then file.Write(window_file, "{}") end
+	local window_file = "homigrad/window_showed.txt"
+	if not file.Exists(window_file, "DATA") then file.Write(window_file, "{}") end
 
-		local window_file_content = file.Read(window_file, "DATA")
-		local tbl = util.JSONToTable(window_file_content)
+	local window_file_content = file.Read(window_file, "DATA")
+	local tbl = util.JSONToTable(window_file_content)
 
-		if not tbl[ply:SteamID64()] then
+	if not tbl[ply:SteamID64()] then
 			-- Send net message to client to show window
-			tbl[ply:SteamID64()] = true
+		tbl[ply:SteamID64()] = true
 
-			net.Start("ShowWelcomeWindow")
-			net.Send(ply)
+		net.Start("ShowWelcomeWindow")
+		net.Send(ply)
 
-			-- Add info that player saw the window
-			file.Write(window_file, util.TableToJSON(tbl))
-		end
-	end)
-end
+		-- Add info that player saw the window
+		file.Write(window_file, util.TableToJSON(tbl))
+	end
+end)
