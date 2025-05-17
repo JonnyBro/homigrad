9c3edf3a39b633675aecd99dd063648625b6c80a
Jonny_Bro <48434875+JonnyBro@users.noreply.github.com>
2025-05-18 00:23:54 +0500

    fix: random error in spawnmenu_sh

--- lua/homigrad_scr/game/tier_1/spawnmenu_sh.lua
+++ lua/homigrad_scr/game/tier_1/spawnmenu_sh.lua
@@ -1,13 +1,15 @@
 local validUserGroup = {
 	superadmin = true,
-	owner = true,
-	servermanager = true
 }
 
 if SERVER then
 	COMMANDS.accessspawn = {
 		function(ply, args)
-			-- SetGlobalBool("AccessSpawn", tonumber(args[1]) > 0)
+			local arg = tobool(args[1])
+			if not arg then arg = false end
+
+			SetGlobalBool("AccessSpawn", arg)
+
 			PrintMessage(3, "Spawn Menu Boolean: " .. tostring(GetGlobalBool("AccessSpawn")))
 		end
 	}
@@ -17,12 +19,8 @@ if SERVER then
 		func = func and func(ply, class)
 		if func ~= nil then return func end
 
-		if validUserGroup[ply:GetUserGroup()] and ply:Team() ~= TEAM_SPECTATOR or GetGlobalBool("AccessSpawn") or GetConVar("hg_ConstructOnly"):GetBool() then return true end
-
-		if not validUserGroup[ply:GetUserGroup()] then
-			ply:Kick("You do not have access to these tools.")
-			return false
-		end
+		if IsValid(ply) and validUserGroup[ply:GetUserGroup()] and ply:Team() ~= TEAM_SPECTATOR or GetGlobalBool("AccessSpawn") or GetConVar("hg_ConstructOnly"):GetBool() then return true
+		else return false end
 	end
 
 	hook.Add("PlayerSpawnVehicle", "Cantspawnbullshit", function(ply) return CanUseSpawnMenu(ply, "vehicle") end)
@@ -46,23 +44,15 @@ if SERVER then
 	hook.Add("PlayerSpawnedSENT", "sv_round", function(ply, model, ent) spawn(ply, "sent", ent) end)
 	hook.Add("PlayerSpawnedNPC", "sv_round", function(ply, model, ent) spawn(ply, "npc", ent) end)
 else
-	-- local admin_menu = CreateClientConVar("hg_admin_menu", "1", true, false, "enable admin menu", 0, 1)
-
 	local function CanUseSpawnMenu()
 		local ply = LocalPlayer()
-
-		if validUserGroup[ply:GetUserGroup()] and ply:Team() ~= TEAM_SPECTATOR or GetConVar("hg_ConstructOnly"):GetBool() then
-			return true
-		else
-			return false
-		end
 
 		local func = TableRound().CanUseSpawnMenu
 		func = func and func(LocalPlayer())
 		if func ~= nil then return func end
 
-		-- if not ply:IsAdmin() then return false end
-		-- if not admin_menu:GetBool() then return false end
+		if IsValid(ply) and validUserGroup[ply:GetUserGroup()] and ply:Team() ~= TEAM_SPECTATOR or GetConVar("hg_ConstructOnly"):GetBool() then return true
+		else return false end
 	end
 
 	hook.Add("ContextMenuOpen", "hide_spawnmenu", CanUseSpawnMenu)
