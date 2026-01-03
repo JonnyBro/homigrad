017a2d55a13f39f370daba27e271a46363107cbf
Sliznya <dimagf13@gmail.com>
2026-01-03 23:08:33 +0200

    added spawn menu icons part1

--- gamemodes/homigrad/entities/weapons/megamedkit/shared.lua
+++ gamemodes/homigrad/entities/weapons/megamedkit/shared.lua
@@ -7,6 +7,7 @@ if CLIENT then
 	SWEP.Author = "Homigrad"
 	SWEP.Instructions = language.GetPhrase("hg.megamedkit.inst")
 	SWEP.Category = language.GetPhrase("hg.category.medicine")
+	SWEP.IconOverride = "materials/items_icons/megamedkiticon.png"
 end
 
 SWEP.Spawnable = true
